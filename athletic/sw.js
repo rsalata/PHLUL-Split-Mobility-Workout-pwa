50dfb9a77228be7a1834c68d2f7bdddea54390ce
rsalata <58432124+rsalata@users.noreply.github.com>
2026-09-21 16:31:03 -0400

    Expose full weekly workouts and running plan

--- athletic/sw.js
+++ athletic/sw.js
@@ -1,4 +1,4 @@
-const CACHE="athletic-bodyweight-5k-v4";
+const CACHE="athletic-bodyweight-5k-v5";
 const SHELL=["./","./index.html","./style.css","./data.js","./demos.js","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
